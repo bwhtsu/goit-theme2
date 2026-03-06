b3ea50b96b7a68b1794f9bdab5036a9e0c00d48a
bwhtsu <liliazhdek@gmail.com>
2026-03-06 14:35:53 +0200

    homework

---
+++ script.js
@@ -0,0 +1,22 @@
+// 1.Створіть слайдер на сторінці, який показує зображення. При переміщенні слайдера виконуйте деякі дії, наприклад, змінюйте розмір зображення. Використайте debounce для того, щоб ці дії виконувалися не занадто часто при швидкому переміщенні слайдера.
+
+
+// const sliderInputRef = document.querySelector('.slider__input');
+// const sliderImageRef = document.querySelector('.slider__image');
+
+// sliderInputRef.addEventListener("input", _.debounce((event) => {
+//     const value = event.target.value;
+//     sliderImageRef.style.width = `${value}%`
+// }, 100));
+
+
+// ================================================================================
+
+// 2.Потрібно забезпечити плавне переміщення об'єкту при русі мишкою. Рішення: використовуйте метод debounce з бібліотеки lodash. Встановіть час затримки в мілісекундах, наприклад 100мс, і передайте функцію, яка буде виконуватися при переміщенні мишкою.
+
+const boxRef = document.getElementById("box");
+
+document.addEventListener('mousemove', _.debounce((event) => {
+    boxRef.style.transform = `translate(${event.clientX}px, ${event.clientY}px)`;
+}, 100));
+
